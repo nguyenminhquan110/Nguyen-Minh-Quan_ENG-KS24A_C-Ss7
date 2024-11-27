ce052e3e0b2ec34efdb36f880a818d98876472fe
nguyenminhquan110 <nmquan11026@gmail.com>
2024-11-28 00:22:53 +0700

    Add files via upload

---
+++ Ss7_4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("Nhap phan tu thu %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+    printf("Cac phan tu trong mang: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
